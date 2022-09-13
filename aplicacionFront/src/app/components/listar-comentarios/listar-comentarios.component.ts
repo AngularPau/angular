5543b873cd9160e108366397ad6f3e1f53652c04
AngularPau <113474203+AngularPau@users.noreply.github.com>
2022-09-13 15:53:11 -0500

    angular
    Proyecto Angular

---
+++ aplicacionFront/src/app/components/listar-comentarios/listar-comentarios.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Comentario } from 'src/app/interfaces/Comentario';
+
+@Component({
+  selector: 'app-listar-comentarios',
+  templateUrl: './listar-comentarios.component.html',
+  styleUrls: ['./listar-comentarios.component.css']
+})
+export class ListarComentariosComponent implements OnInit {
+
+
+  listComentarios: Comentario []=[
+    {
+      titulo: 'Cibersecurity',
+      autor: 'Jorge Collazos',
+      fechaCreacion: new Date(), texto:'Ejemplo',
+    }
+  ]
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
